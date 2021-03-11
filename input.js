f2b4c7413136a12982d28bfbf3a4f831ce4e87cb
Amanda Tuyen <atuyen26@gmail.com>
2021-03-11 08:34:41 +0000

    added connection and key movements

--- input.js
+++ input.js
@@ -1,3 +1,4 @@
+let connection;
 
 const setupInput = function(conn) {
   connection = conn;
@@ -12,6 +13,16 @@ const setupInput = function(conn) {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+    } else if (key === 'd') {
+      connection.write('Move: right');
+    } 
   }
+
 
   module.exports = {setupInput};
